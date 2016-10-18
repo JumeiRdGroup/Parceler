9046310028fd5a101ccea6a5840e102f2007a6b5
liuzhihao <470368500@qq.com>
2016-10-18 18:35:23 +0800

    sample code

---
+++ app/src/main/java/com/lzh/compiler/parcelerdemo/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.lzh.compiler.parcelerdemo;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import com.lzh.compiler.parceler.Parceler;
+import com.lzh.compiler.parceler.annotation.Arg;
+import com.lzh.compiler.parcelerdemo.base.BaseActivity;
+
+import butterknife.Bind;
+import butterknife.OnClick;
+import butterknife.OnTextChanged;
+
+public class LoginActivity extends BaseActivity {
+
+    @Arg
+    String username;
+    @Arg
+    String password;
+
+    @Bind(R.id.username)
+    TextView userTv;
+    @Bind(R.id.password)
+    TextView psdTv;
+    @Bind(R.id.info)
+    TextView info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        userTv.setText(username);
+        psdTv.setText(password);
+    }
+
+    @OnTextChanged(value = R.id.username,callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    void onUserTextChange (Editable editable) {
+        username = editable.toString();
+    }
+    @OnTextChanged(value = R.id.password,callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    void onPsdTextChange (Editable editable) {
+        password = editable.toString();
+    }
+
+    @OnClick(R.id.injectBundle)
+    void onInjectClick () {
+        Bundle bundle = new Bundle();
+        Parceler.injectToData(this,bundle);
+        info.setText(bundle.toString());
+    }
+}
--- app/src/main/java/com/lzh/compiler/parcelerdemo/MainActivity.java
+++ app/src/main/java/com/lzh/compiler/parcelerdemo/MainActivity.java
@@ -2,76 +2,42 @@ package com.lzh.compiler.parcelerdemo;
 
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lzh.compiler.parceler.Parceler;
 import com.lzh.compiler.parceler.annotation.Arg;
+import com.lzh.compiler.parcelerdemo.base.BaseActivity;
 import com.lzh.compiler.parcelerdemo.bean.UserInfo;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.Set;
 
-import static com.lzh.compiler.parcelerdemo.R.id.tv;
+import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends BaseActivity {
     @Arg
-    int position;
+    String password = "112113114";
     @Arg
-    String name;
-    TextView tv;
+    String username = "147258369";
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Parceler.injectToTarget(this,getIntent().getExtras());
-
-        tv = (TextView) findViewById(R.id.tv);
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inject ();
-            }
-        });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void inject() {
+    @OnClick(R.id.toLogin)
+    void onLoginClick () {
         Bundle bundle = new Bundle();
-        UserInfo info = new UserInfo();
-        long start = System.currentTimeMillis();
-        Parceler.injectToData(info,bundle);
-        long end = System.currentTimeMillis();
-        Toast.makeText(this, "注入成功：耗时：" + (end - start), Toast.LENGTH_SHORT).show();
-        StringBuilder buffer = new StringBuilder();
-        Set<String> keySet = bundle.keySet();
-        for (String key : keySet) {
-            Object value = bundle.get(key);
-            assert value != null;
-            buffer.append("value:" + key)
-                    .append("\r\n")
-                    .append("value:" + value)
-                    .append("\r\n")
-                    .append("type:" + value.getClass().getSimpleName())
-                    .append("\r\n\r\n");
-        }
-        tv.setText(buffer.toString());
-    }
-
-    String flat (Object object) {
-        if (object instanceof Object[]) {
-            return Arrays.toString((Object[]) object);
-        }
-        return object.toString();
+        Parceler.injectToData(this,bundle);
+        Intent intent = new Intent(this,LoginActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 
 }
